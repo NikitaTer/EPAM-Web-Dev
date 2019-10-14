4f86f50cd7367d4f3b2b5f8f89c4c4672c4ee0ed
NikiTer <nikitatet100@gmail.com>
2019-10-14 14:00:12 +0300

    created multithreading task

---
+++ MultithreadingTask/src/main/java/by/epam/entity/LogisticBase.java
@@ -0,0 +1,19 @@
+package by.epam.entity;
+
+public class LogisticBase {
+    private static final LogisticBase instance = new LogisticBase();
+
+    private static final int CAPACITY = 5;
+
+    private LogisticBase() {
+
+    }
+
+    public static void load() {
+
+    }
+
+    public static void unload() {
+
+    }
+}
---
+++ MultithreadingTask/src/main/java/by/epam/entity/Van.java
@@ -0,0 +1,8 @@
+package by.epam.entity;
+
+public class Van implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
